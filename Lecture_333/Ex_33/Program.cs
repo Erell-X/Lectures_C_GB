91b142940575adef46eaa1654d0773786ea406b1
*Elizarov <Super.elizarov1451@mail.ru>
2023-05-27 20:57:30 +0300

    Отработка написания кода к задаче3 Лекции3 по C#

---
+++ Lecture_333/Ex_33/Program.cs
@@ -0,0 +1,24 @@
+﻿
+string text = "Ракета испытывает со стороны среды действие сил трения и " 
+            + "давления на поверхности. Значения этих сил и законы их " 
+            + "распределения зависят от скорости полёта. "
+            + "Статическая составляющая сил атмосферного давления " 
+            + "уже была нами учтена при выводе выражения тяги.";
+
+string Replace (string text, char oldSymbol, char newSymbol) // описание метода
+{
+  string result = String.Empty; // введение переменной result, равной пустой строке
+  int length = text.Length;     // введение переменной длины, равной длине текста   
+
+  for (int pro=0; pro<length; pro++)
+  {
+    if (text[pro] == oldSymbol) result = result + $"{newSymbol}";
+    else result = result + $"{text[pro]}";
+  }
+  return result;
+}
+
+string TextPro = Replace (text, 'в', 'В');
+System.Console.WriteLine();
+System.Console.WriteLine(TextPro);
+System.Console.WriteLine();
